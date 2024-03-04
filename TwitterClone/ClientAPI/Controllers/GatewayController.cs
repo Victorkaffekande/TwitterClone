15a3c8adb89f45eeace9a227fd1e159a8a5c8f3b
ChristianHesberg <chri59kx@easv365.dk>
2024-03-04 16:41:11 +0100

    feat: client api can communicate with backend apis

--- TwitterClone/ClientAPI/Controllers/GatewayController.cs
+++ TwitterClone/ClientAPI/Controllers/GatewayController.cs
@@ -5,10 +5,10 @@ namespace ClientAPI.Controllers;
 public class GatewayController : ControllerBase
 {
     private static string authService = "auth-service";
+    private static string timelineService = "timeline-service";
+    private static string tweetService = "tweet-service";
     private HttpClient _client = new();
 
-    
-    
     [HttpGet]
     [Route("test")]
     public ActionResult test()
@@ -19,9 +19,28 @@ public class GatewayController : ControllerBase
     [HttpGet]
     [Route("AuthTest")]
     public ActionResult AuthTest()
-    { 
-        //var res = _client.Send(new HttpRequestMessage(HttpMethod.Get, "Auth/test"));
-        var res = _client.Send(new HttpRequestMessage(HttpMethod.Get, "http://auth-service/Auth/test"));
+    {
+        var res = _client.Send(new HttpRequestMessage(HttpMethod.Get, $"http://{authService}/auth/test"));
+        
+        var result = res.Content.ReadAsStringAsync().Result;
+        return Ok(result);
+    }
+    
+    [HttpGet]
+    [Route("TimelineTest")]
+    public ActionResult TimelineTest()
+    {
+        var res = _client.Send(new HttpRequestMessage(HttpMethod.Get, $"http://{timelineService}/timeline/test"));
+        
+        var result = res.Content.ReadAsStringAsync().Result;
+        return Ok(result);
+    }
+    
+    [HttpGet]
+    [Route("TweetTest")]
+    public ActionResult TweetTest()
+    {
+        var res = _client.Send(new HttpRequestMessage(HttpMethod.Get, $"http://{tweetService}/tweet/test"));
         
         var result = res.Content.ReadAsStringAsync().Result;
         return Ok(result);
